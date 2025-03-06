162a1ec2d59261a96d1883302320277ba8207077
Apramay <142184694+Apramay@users.noreply.github.com>
2025-03-06 12:32:23 -0500

    Update game.js

--- game.js
+++ game.js
@@ -32,8 +32,8 @@ function dealHand(deck, numCards) {
     return hand;
 }
 function displayCard(card) {
-    const rank = card.rank;
-    const suit = card.suit;
+    const rank = card.rank.toLowerCase();
+    const suit = card.suit.toLowerCase();
     const imageName = `${rank}_of_${suit}.png`;
 
     return `<img src="./cards/${imageName}" alt="${rank} of ${suit}" onerror="this.onerror=null; this.src='./cards/default.png';">`;
