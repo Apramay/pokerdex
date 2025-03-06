7a75802098920fa5b87b78c20600b5de21cf309a
Apramay <142184694+Apramay@users.noreply.github.com>
2025-03-06 12:16:55 -0500

    Update game.js

--- game.js
+++ game.js
@@ -35,9 +35,10 @@ function dealHand(deck, numCards) {
 function displayCard(card) {
     const rank = card.rank;
     const suit = card.suit;
-    const imageName = `${rank}_of_${suit}.png`;
+    const imageUrl = `https://raw.githubusercontent.com/Apramay/pokerdex/main/cards/${imageName}`;
 
-    return `<img src="./cards/${imageName}" alt="${rank} of ${suit}" onerror="this.onerror=null; this.src='./cards/default.png';">`;
+    return `<img src="${imageUrl}" alt="${rank} of ${suit}" onerror="this.onerror=null; this.src='./cards/default.png';">`;
+}
 }
 
 
