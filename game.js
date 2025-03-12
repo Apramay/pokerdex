d4a3a2d0c5e0ebe7a17893a611ede71bd0a4cdf7
Apramay <142184694+Apramay@users.noreply.github.com>
2025-03-12 12:22:05 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 const rankValues = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14 };
 
 function createDeck() {
-    const deck =;
+    const deck = [];
     for (const suit of suits) {
         for (const rank of ranks) {
             deck.push({ suit, rank });
@@ -25,7 +25,7 @@ function dealCard(deck) {
 }
 
 function dealHand(deck, numCards) {
-    const hand =;
+    const hand = [];
     for (let i = 0; i < numCards; i++) {
         hand.push(dealCard(deck));
     }
